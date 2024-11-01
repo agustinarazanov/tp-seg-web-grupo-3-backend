c37be401f293cadda3d5d8f7d66442dc9d1163f0
IgnacioFernandez5361 <ignaciofernandez5361@gmail.com>
2024-11-01 00:50:22 -0300

    SQL Injection en POST de Users

--- src/main/java/saw/controllers/UserController.java
+++ src/main/java/saw/controllers/UserController.java
@@ -1,6 +1,13 @@
 package saw.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
 import saw.exceptions.UserNotFoundException;
 import saw.models.User;
 import saw.repositories.UserRepository;
@@ -16,14 +23,23 @@ public class UserController {
         this.repository = repository;
     }
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @GetMapping("/users")
     public List<User> all() {
         return repository.findAll();
     }
 
+    @Transactional
     @PostMapping("/users")
-    public User newUser(@RequestBody User user) {
-        return repository.save(user);
+    public void newUser(@RequestBody User user) {
+        entityManager.createNativeQuery("INSERT INTO user (email, password, name, role) VALUES ('" 
+            + user.getEmail() + "','" + passwordEncoder.encode(user.getPassword()) + "','" + user.getName() + "','" + user.getRole() + "');",
+            User.class).executeUpdate();
     }
 
     @GetMapping("/users/{id}")
